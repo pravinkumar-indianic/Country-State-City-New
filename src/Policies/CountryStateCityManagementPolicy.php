4ef5c59bacc6ed0e3165e536839d47c41ff5afeb
pravinkumar-indianic <96461714+pravinkumar-indianic@users.noreply.github.com>
2023-02-02 14:53:10 +0530

    Update CountryStateCityManagementPolicy.php

--- src/Policies/CountryStateCityManagementPolicy.php
+++ src/Policies/CountryStateCityManagementPolicy.php
@@ -58,7 +58,7 @@ class CountryStateCityManagementPolicy
      *
      * @return Response|bool
      */
-    public function delete(): Response|bool
+    public function delete(Admin $user): Response|bool
     {
         return $user->hasPermissionTo('delete country-management');
     }
